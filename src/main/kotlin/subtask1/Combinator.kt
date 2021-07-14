4b85a4fe5732daae870cc62ca93e5db02c981489
slashbrest <slashbrest@gmail.com>
2021-07-14 22:50:59 +0300

    complete task

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,20 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0]
+        val n = array[1]
+        for (c in 1..n) {
+            if(m * 1L == factorial(n) / (factorial(c) * factorial(n - c))) {
+                return c
+            }
+        }
+        return null
+    }
+    fun factorial (a: Int): Long {
+        var result = 1L
+        for (i in 1..a) {
+            result *= i
+        }
+        return result
     }
 }
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,29 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        if (number >= 5) {
+            return decompose (number * number, number - 1)
+        } else {
+            return null
+        }
+    }
+    private fun decompose (nNumber: Int, nextStep: Int): Array<Int>? {
+        for ( i in nextStep downTo sqrt((nextStep * nextStep / 2).toDouble()).toInt() + 1) {
+            val remain = nNumber - i * i
+            if (remain == 0) {
+                return arrayOf(i)
+            } else {
+                val next = sqrt(remain.toDouble()).toInt()
+                val nextElement = if(next < i) decompose(remain,next) else decompose(remain, i - 1)
+                if (!nextElement.isNullOrEmpty())
+                    return nextElement + arrayOf(i)
+            }
+        }
+        return null
     }
 }
